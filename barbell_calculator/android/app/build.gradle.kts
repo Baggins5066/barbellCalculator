2c070d67e92e693a258b4b189c38d99abaf902dd
Jasper Pell <little0jasper@gmail.com>
2025-05-19 16:35:34 -0400

    Made improvements towards getting app publishable

--- barbell_calculator/android/app/build.gradle.kts
+++ barbell_calculator/android/app/build.gradle.kts
@@ -30,11 +30,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("C:/Users/aiden/weightCalc/barbell_calculator/upload-keystore.jks")
+            storePassword = "JasperBarbell"
+            keyAlias = "upload"
+            keyPassword = "JasperBarbell"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
