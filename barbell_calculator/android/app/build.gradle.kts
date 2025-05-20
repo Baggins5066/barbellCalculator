999c8c3691b128aea6244308a052969da1fa2941
Jasper Pell <little0jasper@gmail.com>
2025-05-19 22:03:45 -0400

    Made app publishable

--- barbell_calculator/android/app/build.gradle.kts
+++ barbell_calculator/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.barbell_calculator"
+    namespace = "com.jasper.barbell_calculator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.barbell_calculator"
+        applicationId = "com.jasper.barbell_calculator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
